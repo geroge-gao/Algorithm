44d97eaa857ef950f7b0cd7a241826de0cabedd0
geroge-gao <1375641052@qq.com>
2018-07-05 21:11:52 +0800

    add 60~61

---
+++ Coding Interviews/60_按行打印二叉树.cpp
@@ -0,0 +1,46 @@
+/*
+
+按行打印二叉树
+author： gerogegao
+date:2018.7.5
+
+*/
+
+vector<vector<int> > Print(TreeNode* pRoot)
+{
+     vector<int> level;
+     vector<vector<int> > array;
+     if(pRoot==NULL)
+        return array;
+    queue<TreeNode*> s;
+    s.push(pRoot);
+    int nextLevel=0;//统计每一层的个数
+    int toBePrinted=1;
+
+    while(!s.empty())
+    {
+        TreeNode *p=s.front();
+        level.push_back(p->val);
+        if(p->left)
+        {
+            s.push(p->left);
+            nextLevel++;
+        }
+        if(p->right)
+        {
+            s.push(p->right);
+            nextLevel++;
+        }
+        s.pop();
+        toBePrinted--;
+        if(toBePrinted==0)
+        {
+            toBePrinted=nextLevel;
+            array.push_back(level);
+            nextLevel=0;
+            level.clear();
+        }
+
+    }
+    return array;
+ }
