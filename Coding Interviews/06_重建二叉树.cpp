d2c8bd17411986e5cc8f316a4c09a790590d70c3
gzj_1101 <1375641052@qq.com>
2018-09-14 09:37:23 +0800

    Update 06_重建二叉树.cpp

--- Coding Interviews/06_重建二叉树.cpp
+++ Coding Interviews/06_重建二叉树.cpp
@@ -1,50 +1,28 @@
-链接：https://www.nowcoder.com/questionTerminal/8a19cbe657394eeaac2f6ea9b0f6fcf6
-来源：牛客网
+TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
 
-        int in_size = in.size();
- 
-        if(in_size == 0)
- 
-            return NULL;
- 
-        vector<int> pre_left, pre_right, in_left, in_right;
- 
-        int val = pre[0];
- 
-        TreeNode* node = new TreeNode(val);//root node is the first element in pre
- 
-        int p = 0;
- 
-        for(p; p < in_size; ++p){
- 
-            if(in[p] == val) //Find the root position in in 
- 
-                break;
- 
-        }
- 
-        for(int i = 0; i < in_size; ++i){
- 
-            if(i < p){
- 
-                in_left.push_back(in[i]);//Construct the left pre and in 
- 
-                pre_left.push_back(pre[i+1]);
- 
-            }
- 
-            else if(i > p){
- 
-                in_right.push_back(in[i]);//Construct the right pre and in 
- 
-                pre_right.push_back(pre[i]);
- 
-            }
- 
-        }
- 
-        node->left = reConstructBinaryTree(pre_left, in_left);
- 
-        node->right = reConstructBinaryTree(pre_right, in_right);
- 
-        return node;
+        int in_size = vin.size(); 
+        if(in_size == 0) 
+            return NULL; 
+        vector<int> pre_left, pre_right, in_left, in_right; 
+        int val = pre[0]; 
+        TreeNode* node = new TreeNode(val);//root node is the first element in pre 
+        int p = 0; 
+        for(p; p < in_size; ++p){ 
+            if(vin[p] == val) //Find the root position in in  
+                break; 
+        }
+ 
+        for(int i = 0; i < in_size; ++i){ 
+            if(i < p){ 
+                in_left.push_back(vin[i]);//Construct the left pre and in  
+                pre_left.push_back(pre[i+1]); 
+            } 
+            else if(i > p){ 
+                in_right.push_back(vin[i]);//Construct the right pre and in  
+                pre_right.push_back(pre[i]); 
+            } 
+        } 
+        node->left = reConstructBinaryTree(pre_left, in_left); 
+        node->right = reConstructBinaryTree(pre_right, in_right); 
+        return node;        
+    }
